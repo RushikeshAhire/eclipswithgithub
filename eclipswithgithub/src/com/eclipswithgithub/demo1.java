69eb9911d9abe96a05b5e5ed908cc97ea3b577e4
Rushi1007 <53400911+Rushi1007@users.noreply.github.com>
2022-12-21 10:56:06 +0530

    changes in my first project by secound eclips

--- eclipswithgithub/src/com/eclipswithgithub/demo1.java
+++ eclipswithgithub/src/com/eclipswithgithub/demo1.java
@@ -5,6 +5,6 @@ public class demo1 {
   public static void main(String args[]) {
 	  
 	  System.out.println("Hi this is trail of eclips with github");
-	  
+	 System.out.println("i am changing this one is my secound file");
   }
 }
